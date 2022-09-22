181cf9747310c9efe828f099d0e7634f60ce0c6f
Roy <roycurechean@gmail.com>
2022-09-22 15:23:36 +0200

    pep8 fixes

--- main.py
+++ main.py
@@ -5,44 +5,45 @@ TTL = 3  # time to live for saving calculated values
 
 # binary search a list of lists
 # index is the index of the inner list item
-def binary_search_list(list, index=0, key=0):
-    # list is a list of tupels that contain tupel of keys and ttl
+def binary_search_list(arr, index=0, key=0):
+    # arr is a list of tuples that contain tuple of keys and ttl
     curr_time = time.time()
-    i = int(len(list) / 2)
-    if not list:
+    i = int(len(arr) / 2)
+    if not arr:
         return -1
     while i > 0:
-        if curr_time - list[i][index] < key:
+        if curr_time - arr[i][index] < key:
             i = int(i + i / 2)
-        elif curr_time - list[i][index] > key:
+        elif curr_time - arr[i][index] > key:
             i = int(i - i / 2)
         else:
             return i
-        if curr_time - list[i][index] < key:
+        if curr_time - arr[i][index] < key:
             return i
-    if curr_time - list[0][index] < key:
+    if curr_time - arr[0][index] < key:
         return 0
     return -1
 
 
 # cleans all items in list starting from index i
-def clean_expired_items(list, i, ttl, vals):
+def clean_expired_items(arr, i, ttl, vals):
     while i >= 0:
-        vals.pop(list[i][2])
-        ttl.pop(list[i][2])
-        list.pop(i)
+        vals.pop(arr[i][2])
+        ttl.pop(arr[i][2])
+        arr.pop(i)
         i -= 1
 
-# resets the TTL of list[i] in all the relevant data structures (list, ttl_dict)
-def reset_ttl(list, i, ttl_dict):
-    temp = list.pop(i)
+
+# resets the TTL of arr[i] in all the relevant data structures (arr, ttl_dict)
+def reset_ttl(arr, i, ttl_dict):
+    temp = arr.pop(i)
     curr_time = time.time()
     ttl_dict[temp[2]] = curr_time
-    list.append((temp[0], curr_time, temp[2]))
-    return list[-1][0]
+    arr.append((temp[0], curr_time, temp[2]))
+    return arr[-1][0]
 
 
-#  decorator function that apllies cache with TTL on every function that it wraps
+#  decorator function that applies cache with TTL on every function that it wraps
 
 def decorator(original_func):
     funcs = {}
@@ -87,7 +88,7 @@ print(square(2))
 print(square(3))
 print(square(3))
 print(square(3))
-print(add(5,7))
+print(add(5, 7))
 print(square(2))
 time.sleep(3.1)
 print(square(2))
